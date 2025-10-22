a47f6b3a83677383a99d51229532f0cd5e2efd11
Swastik Mishra <swastik.mishra201@gmail.com>
2025-10-22 19:19:58 +0530

    Initial commit - Smart E-Buggy

---
+++ Main.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Test route (to verify deployment works)
+@app.route("/")
+def home():
+    return "Smart E-Buggy Tracker is LIVE ✅"
+
+# API endpoint where ESP32 will send data later
+@app.route("/api/location", methods=["POST"])
+def receive_location():
+    data = request.get_json()
+    print("Received:", data)
+    return jsonify({"status": "ok", "message": "data received", "data": data})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
